cdab6774566d13ad0dc55a1ee22c6ba4aa5a888a
Maison Armani <masonarmani38@gmail.com>
2020-07-22 12:52:49 +0100

    remove account found check

--- erpnext_custom/custom_accounts/report/custom_balance_sheet/custom_balance_sheet.py
+++ erpnext_custom/custom_accounts/report/custom_balance_sheet/custom_balance_sheet.py
@@ -94,9 +94,6 @@ def execute(filters=None):
 	# Asset get debit balance of all specified accounts
 	# Liability get the credit balance of all specified accounts
 	remove_group_accounts(grouper)
-
-
-
 	configuration = frappe.get_doc('Financial Report Configuration', "BSH002")
 	grouping, group = dict(), None
 
@@ -364,22 +361,17 @@ def calculate_values(
 	for entries in itervalues(gl_entries_by_account):
 		for entry in entries:
 			d = accounts_by_name.get(entry.account)
-			if not d:
-				frappe.msgprint(
-					_("Could not retrieve information for {0}.".format(entry.account)), title="Error",
-					raise_exception=1
-				)
-			for period in period_list:
-				# check if posting date is within the period
-				if entry.posting_date <= period.to_date:
-					if (accumulated_values or entry.posting_date >= period.from_date) and \
-						(not ignore_accumulated_values_for_fy or
-							entry.fiscal_year == period.to_date_fiscal_year):
+			if  d:
+				for period in period_list:
+					# check if posting date is within the period
+					if entry.posting_date <= period.to_date:
+						if (accumulated_values or entry.posting_date >= period.from_date) and \
+							(not ignore_accumulated_values_for_fy or
+								entry.fiscal_year == period.to_date_fiscal_year):
 
-						d[period.key] = d.get(period.key, 0.0) + flt(entry.debit) - flt(entry.credit)
-						d[period.key+"_debit"] = d.get(period.key+"_debit", 0.0) + flt(entry.debit)
-						d[period.key+"_credit"] = d.get(period.key+"_credit", 0.0) + flt(entry.credit)
+							d[period.key] = d.get(period.key, 0.0) + flt(entry.debit) - flt(entry.credit)
+							d[period.key+"_debit"] = d.get(period.key+"_debit", 0.0) + flt(entry.debit)
+							d[period.key+"_credit"] = d.get(period.key+"_credit", 0.0) + flt(entry.credit)
 
-
-			if entry.posting_date < period_list[0].year_start_date:
-				d["opening_balance"] = d.get("opening_balance", 0.0) + flt(entry.debit) - flt(entry.credit)
+				if entry.posting_date < period_list[0].year_start_date:
+					d["opening_balance"] = d.get("opening_balance", 0.0) + flt(entry.debit) - flt(entry.credit)
